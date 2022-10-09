3c1941f06f89e9ed3d5cce3cf2eb4e2db38a5a52
AmundDynna <amund.dynna@gmail.com>
2022-10-10 00:51:17 +0200

    Fixed some stuff and made entity class

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,9 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 #include <iostream>
+
 #include "RenderWindow.hpp"
+#include "Entity.hpp"
 
 int main(int argc, char* args[])
 {
@@ -13,6 +15,8 @@ int main(int argc, char* args[])
 
     RenderWindow window("GAME v1.0", 500, 300);
 
+    SDL_Texture* grassTexture = window.loadTexture("res/gfx/grass.png");
+
     bool gameRunning = true;
     SDL_Event event;
 
@@ -23,7 +27,10 @@ int main(int argc, char* args[])
             if (event.type == SDL_QUIT)
                 gameRunning = false;
         }
-    }
+        window.clear();
+        window.render(grassTexture);
+        window.display();
+    }   
     // Here we add a comment to test gitlens
     window.cleanUp();
     SDL_Quit();
--- src/renderwindow.cpp
+++ src/renderwindow.cpp
@@ -15,7 +15,43 @@ RenderWindow::RenderWindow(const char* p_title, int p_w, int p_h)
     renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 }
 
+SDL_Texture* RenderWindow::loadTexture(const char* p_filePath)
+{
+    SDL_Texture* texture = NULL;
+    texture = IMG_LoadTexture(renderer, p_filePath);
+    if (texture == NULL)
+        std::cout << "Failed to load texture. Error: " << SDL_GetError() << std::endl;
+    return texture;
+}
+
 void RenderWindow::cleanUp()
 {
     SDL_DestroyWindow(window);
+}
+
+void RenderWindow::clear()
+{
+    SDL_RenderClear(renderer);
+}
+
+void RenderWindow::render(SDL_Texture* p_tex)
+{
+    SDL_Rect src;
+    src.x = 5;
+    src.y = 5;
+    src.w = 16;
+    src.h = 16;
+
+    SDL_Rect dst;
+    dst.x = 400;
+    dst.y = 200;
+    dst.w = 16;
+    dst.h = 16;
+
+    SDL_RenderCopy(renderer, p_tex, NULL, &dst);
+}
+
+void RenderWindow::display()
+{
+    SDL_RenderPresent(renderer);
 }
